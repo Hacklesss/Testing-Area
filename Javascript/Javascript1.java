89778e3966a25ad00f262313528ad61d3de6b7cb
ismail ulusoy <hackless.i.u.41@gmail.com>
2016-02-06 16:53:08 +0200

    Create Javascript1.java
    My first javascript try :)

---
+++ Javascript/Javascript1.java
@@ -0,0 +1,30 @@
+<html>
+  
+<script type="text/javascript" language="javascript">
+
+if ("Jon".length * 2 / (2+1) === 3 )
+
+{
+   console.log("The answer makes sense!");
+} 
+
+else 
+
+{
+   
+   prompt("DOĞRU CEVABI YAZ OC...");
+   
+}
+   if ("2")
+   {
+       
+       console.log("doğru bildin aq");
+   }
+   
+   else
+   
+   {
+       console.log("ölamk");
+   }
+ 
+</script>
