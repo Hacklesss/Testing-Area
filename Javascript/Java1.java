38b08d42a65155ba095e3a2d8f4e8718445a1116
ismail ulusoy <hackless.i.u.41@gmail.com>
2016-02-06 16:51:51 +0200

    Create Java1.java
    My first testing of java

---
+++ Javascript/Java1.java
@@ -0,0 +1,19 @@
+public class Comments {
+	public static void main(String[] args) {
+
+		//System.out.println("Noise!");
+    /*
+    This is multi-line comment written by me. asdasdasd
+    bune olm
+    asdasdasd
+    sen batan asagi kod olsan nolur 
+    sen komple kod olsan nolur
+    */
+		
+
+
+
+
+		
+	}
+}
